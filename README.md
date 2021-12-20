f8198596fefaa72cd77bd9c5ecd8d10e8c5cb3b3
b9024555 <Spore1875>
2021-12-20 14:49:02 +0000

    added testing file for Navbar

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Setup:
 - run ```npm i && npm start``` for both client and server side to start the website
+
+
---
+++ client/src/components/Navbar/Navbar.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from './Navbar';
+import react from 'react';
+import { ExpansionPanelActions } from '@material-ui/core';
+
+test('Navbar is rendered', () => {
+    render('Navbar');
+})
+
